5208a0cf548ebebd369f93ba7136786375a3f748
BGoncalvess <goncalves02bruno@gmail.com>
2025-02-21 19:42:09 +0000

    refactor: Initial Structure

--- src/ActionButton.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-from Button import Button
-
-class ActionButton(Button):
-    def __init__(self, text, button_clicked):
-        Button.__init__(self, text, button_clicked)
-        self.bgcolor = ft.colors.ORANGE
-        self.color = ft.colors.WHITE
--- src/DigitButton.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-from Button import Button
-
-class DigitButton(Button):
-    def __init__(self, text, button_clicked, expand=1):
-        Button.__init__(self, text, button_clicked, expand)
-        self.bgcolor = ft.colors.WHITE24
-        self.color = ft.colors.WHITE
