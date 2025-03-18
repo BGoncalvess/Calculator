c9529a49ecf517ca25169b9f6424bcb86ce5a4e0
Goncalves <brgoncalves@deloitte.pt>
2025-03-18 20:10:46 +0000

    feat: Update app initialization to remove host specification while retaining web browser mode and port configuration

--- Calculator/src/main.py
+++ Calculator/src/main.py
@@ -12,4 +12,5 @@ def main(page: ft.Page):
     page.window.width=400
     page.window.height=500
     
-ft.app(target=main, view=ft.WEB_BROWSER, host="0.0.0.0", port=8080)
+# ft.app(target=main)
+ft.app(target=main, view=ft.WEB_BROWSER, host="0.0.0.0", assets_dir=, port=8080)
