1705cda4402c638d9ca025838eba71005b9f9eb9
Goncalves <brgoncalves@deloitte.pt>
2025-03-18 22:50:20 +0000

    feat: Refactor app initialization for improved readability and structure

--- Calculator/src/main.py
+++ Calculator/src/main.py
@@ -2,15 +2,19 @@ import flet as ft
 from core import RouteManager
 from storage.HistoryStorage import HistoryStorage
 
+
 def main(page: ft.Page):
-    
+
     page.title = "Calculator App"
-    
+
     HistoryStorage.initialize(page)
     RouteManager.initialize_route_manager(page)
 
-    page.window.width=400
-    page.window.height=500
-    
-# ft.app(target=main)
-ft.app(target=main, view=ft.WEB_BROWSER, host="0.0.0.0", assets_dir=, port=8080)
+    page.window.width = 400
+    page.window.height = 500
+
+
+ft.app(target=main,
+       view=ft.AppView.WEB_BROWSER,
+       assets_dir="assets",
+       port=8080)
